4a4fb204ba77126c391121f8aff83cd4577adc09
mirandalily <mirandaraymond@me.com>
2017-06-20 16:49:56 -0700

    accidentally put 1 instead of i

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,13 +1,15 @@
 # Implement your object-oriented solution here!
 class SumSquareDifference
-  
+  def initialize(num)
+    @num = num
+  end
 
-  def difference(num)
+  def difference
     square_sums = 0
     sum = 0
-    for i in 1..num
-      square_sums += 1 * i
-      sum += 1
+    for i in 1..@num
+      square_sums += i * i
+      sum += i
     end
     return (sum * sum) - square_sums
   end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,3 +1,10 @@
 # Implement your procedural solution here!
-def sum_square
+def sum_square_difference(num)
+  square_sums = 0
+  sum = 0
+  for i in 1..num
+    square_sums += i * i
+    sum += i
+  end
+  return (sum * sum) - square_sums
 end
