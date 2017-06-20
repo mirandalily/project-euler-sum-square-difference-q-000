39199926af68100f1e2096bbd7b3fb67ba682883
mirandalily <mirandaraymond@me.com>
2017-06-20 16:46:48 -0700

    no tests passing yet

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,10 +1,15 @@
 # Implement your object-oriented solution here!
 class SumSquareDifference
-  def initialize(num)
-    @num = num
+  
+
+  def difference(num)
+    square_sums = 0
+    sum = 0
+    for i in 1..num
+      square_sums += 1 * i
+      sum += 1
+    end
+    return (sum * sum) - square_sums
   end
 
-  def difference
-  end
-  
 end
